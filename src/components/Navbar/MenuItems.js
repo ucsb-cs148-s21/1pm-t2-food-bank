9a8172bf77bc1a962699aa9bac80cba6a3ee9811
Liuhao Wu <liuhaowu@ucsb.edu>
2021-04-20 11:53:25 -0700

    lw - add basic navbar

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Student Services',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contact Us',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Staff Login',
+        url: '#',
+        cName: 'nav-links-mobile'
+    }
+]
