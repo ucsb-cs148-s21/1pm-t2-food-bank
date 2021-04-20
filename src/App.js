43e42cbfa9ecc64f76fd273eff8095cede364847
Liuhao Wu <liuhaowu@ucsb.edu>
2021-04-20 09:26:44 -0700

    lw - blank project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>Hello World!</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
-      </header>
+
     </div>
   );
 }
