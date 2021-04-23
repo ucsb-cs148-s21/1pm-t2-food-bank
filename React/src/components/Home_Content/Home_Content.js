089f404e8ec50b431204d0b7a7f513497f18a4c6
Jerry Wang <tianchenwang@ucsb.edu>
2021-04-23 08:55:35 +0800

    add main page content

--- React/src/components/Home_Content/Home_Content.js
+++ React/src/components/Home_Content/Home_Content.js
@@ -14,7 +14,7 @@ class Home_Content extends Component {
                 <div className="img_AS-Food-bank"></div>
                 <ul>
                     <li><div className="introduction_paragraph">This website will let you know what's in the food bank today</div></li>
-                    <li><iframe className="Introduction_YT" src="https://www.youtube.com/embed/GgJJRo5uT0c"></iframe></li>
+                    <li><center><iframe className="Introduction_YT" src="https://www.youtube.com/embed/GgJJRo5uT0c"></iframe></center></li>
                     <li><div className="register_paragraph">Prior to your first visit, please&nbsp;<a href="http://tinyurl.com/asfbreg">register </a>first.</div></li>
                 </ul>
             </div>
