87e4bb48cae94d2b7b22b4f67a1a91ced98e754c
kaiwenli1 <kaiwen_li@umail.ucsb.edu>
2021-05-06 20:49:38 -0700

    add limit per person on food

--- src/main/java/t2foodbank/FoodBank/Components/HomeController.java
+++ src/main/java/t2foodbank/FoodBank/Components/HomeController.java
@@ -67,6 +67,4 @@ public class HomeController {
         return "Delete inventory "+name;
     }
 
-    
-
 }
--- src/main/java/t2foodbank/FoodBank/objects/Food.java
+++ src/main/java/t2foodbank/FoodBank/objects/Food.java
@@ -6,17 +6,26 @@ import org.springframework.stereotype.Component;
 public class Food {
 
    private String name;
+   private int limit;
 
+   public int getLimit() {
+      return this.limit;
+   }
+
+   public void setLimit(int limit) {
+      this.limit = limit;
+   }
    private int amount;
 
    public Food (){
        super();
    }
 
-   public Food(String name, int amount) {
+   public Food(String name, int amount, int limit) {
       super();
       this.name = name;
       this.amount = amount;
+      this.limit = limit;
    }
 
    public String getName() {
