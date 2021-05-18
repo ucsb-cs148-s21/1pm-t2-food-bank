9ff37ec25e124563d21818cf44209aa007644b0e
Orion Schwellenbach <jamesschwellenbach@ucsb.edu>
2021-05-17 21:56:21 -0700

    initial version

---
+++ React/src/components/Staff/Staff.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import BootstrapTable from 'react-bootstrap-table-next';
+import './Staff.css'
+
+const columns = [{
+  dataField: 'category',
+  text: 'Category'
+  
+},
+{
+  dataField: 'name',
+  text: 'Inventory'
+},
+{
+  dataField: 'amount',
+  text: 'Quantity'
+},
+{  
+  dataField: 'limit',
+  text: 'Limit'
+}];
+
+
+export default class Inventory extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inventory: null,
+      loading: true,
+      error: null
+    };
+  }
+
+  async componentDidMount() {
+    const API = "/api/getInventory";
+    const response = await fetch(API);
+    try {
+      const data = await response.json();
+      this.setState({ inventory: data, loading: false });
+    } catch (e) { this.setState({ error: e }) }
+  }
+
+  render() {
+    if (this.state.error) {
+      return <p>{this.state.error.message}</p>;
+    }
+    if (this.state.loading) {
+      return <p>Loading ...</p>;
+    }
+    return (
+      <div className='inventoryTable'>
+          <BootstrapTable  keyField='catagory' data={this.state.inventory || []} columns={columns} />
+      </div>
+    );
+  }
+}
