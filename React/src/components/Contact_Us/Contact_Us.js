ad2e2987878358ccb2598e75229458384494ed9b
Liuhao Wu <liuhaowu@ucsb.edu>
2021-05-20 11:09:28 -0700

    beautify contactus using bootstrap v4.6.0, change inventory searchbar css, and comment out login page css

--- React/src/components/Contact_Us/Contact_Us.js
+++ React/src/components/Contact_Us/Contact_Us.js
@@ -1,43 +1,146 @@
-import React from 'react';
+import { useForm } from 'react-hook-form';
 import emailjs from 'emailjs-com';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
 
-export default function ContactUs() {
+import './Contact_Us.css';
+import './v4.6.0.css';
 
-  function sendEmail(e) {
-    e.preventDefault();
+const ContactForm = () => {
+  const { register, formState: { errors }, handleSubmit, reset } = useForm();
 
-    emailjs.sendForm('service_ct6th7q', 'template_yy4g439', e.target, 'user_mBRJGCFBaEf74unMoMZE2')
-      .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
-      });
-      e.target.reset();
-  }
+  const toastifySuccess = () => {
+    toast('Form sent!', {
+      position: 'bottom-right',
+      autoClose: 5000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: false,
+      className: 'submit-feedback success',
+      toastId: 'notifyToast'
+    });
+  };
 
-  return(
-    <div>
-        <div className="container">
-        <form onSubmit={sendEmail}>
-                <div className="row pt-5 mx-auto">
-                    <div className="col-8 form-group mx-auto">
-                        <input type="text" className="form-control" placeholder="Name" name="name"/>
-                    </div>
-                    <div className="col-8 form-group pt-2 mx-auto">
-                        <input type="email" className="form-control" placeholder="Email Address" name="email"/>
-                    </div>
-                    <div className="col-8 form-group pt-2 mx-auto">
-                        <input type="text" className="form-control" placeholder="Subject" name="subject"/>
-                    </div>
-                    <div className="col-8 form-group pt-2 mx-auto">
-                        <textarea className="form-control" id="" cols="30" rows="8" placeholder="Your message" name="message"></textarea>
-                    </div>
-                    <div className="col-8 pt-3 mx-auto">
-                        <input type="submit" className="btn btn-info" value="Send Message"></input>
-                    </div>
+  const onSubmit = async (data) => {
+    // Send form email
+    try {
+      const templateParams = {
+        name: data.name,
+        email: data.email,
+        subject: data.subject,
+        message: data.message
+      };
+
+      await emailjs.send(
+        'service_ct6th7q',
+        'template_yy4g439',
+        templateParams,
+        'user_mBRJGCFBaEf74unMoMZE2'
+
+        // .env
+        // process.env.REACT_APP_SERVICE_ID,
+        // process.env.REACT_APP_TEMPLATE_ID,
+        // templateParams,
+        // process.env.REACT_APP_USER_ID
+      );
+
+      reset();
+      toastifySuccess();
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <div className='ContactForm'>
+      <div className='container'>
+      <h1 className='title'> Contact Form </h1>
+        <div className='row'>
+          <div className='col-12 text-center'>
+            <div className='contactForm'>
+              <form id='contact-form' onSubmit={handleSubmit(onSubmit)} noValidate>
+                {/* Row 1 of form */}
+                <div className='row formRow'>
+                  <div className='col-6'>
+                    <input
+                      type='text'
+                      // name='name'
+                      {...register('name', {
+                        required: { value: true, message: 'Please enter your name' },
+                        maxLength: {
+                          value: 30,
+                          message: 'Please use 30 characters or less'
+                        }
+                      })}
+                      className='form-control formInput'
+                      placeholder='Name'
+                    ></input>
+                    {errors.name && <span className='errorMessage'>{errors.name.message}</span>}
+                  </div>
+                  <div className='col-6'>
+                    <input
+                      type='email'
+                      // name='email'
+                      {...register('email', {
+                        required: true,
+                        pattern: /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+                      })}
+                      className='form-control formInput'
+                      placeholder='Email address'
+                    ></input>
+                    {errors.email && (
+                      <span className='errorMessage'>Please enter a valid email address</span>
+                    )}
+                  </div>
                 </div>
-            </form>
+                {/* Row 2 of form */}
+                <div className='row formRow'>
+                  <div className='col'>
+                    <input
+                      type='text'
+                      // name='subject'
+                      {...register('subject', {
+                        required: { value: true, message: 'Please enter a subject' },
+                        maxLength: {
+                          value: 75,
+                          message: 'Subject cannot exceed 75 characters'
+                        }
+                      })}
+                      className='form-control formInput'
+                      placeholder='Subject'
+                    ></input>
+                    {errors.subject && (
+                      <span className='errorMessage'>{errors.subject.message}</span>
+                    )}
+                  </div>
+                </div>
+                {/* Row 3 of form */}
+                <div className='row formRow'>
+                  <div className='col'>
+                    <textarea
+                      rows={3}
+                      // name='message'
+                      {...register('message', {
+                        required: true
+                      })}
+                      className='form-control formInput'
+                      placeholder='Message'
+                    ></textarea>
+                    {errors.message && <span className='errorMessage'>Please enter a message</span>}
+                  </div>
+                </div>
+                <button className='submit-btn' type='submit'>
+                  Submit
+                </button>
+              </form>
+            </div>
+            <ToastContainer />
+          </div>
         </div>
+      </div>
     </div>
-)
-}
+  );
+};
+
+export default ContactForm;
