51177b5c36b6e3077356208e8295e96af0c915a8
Liuhao Wu <liuhaowu@ucsb.edu>
2021-05-24 20:36:04 -0700

    fixed login button in logged-out mode

--- React/src/components/Navbar/Navbar.js
+++ React/src/components/Navbar/Navbar.js
@@ -1,3 +1,58 @@
+import React, { useState } from 'react';
+
+import './Navbar.css'
+import { Link } from 'react-router-dom';
+
+import Container from "react-bootstrap/Container";
+import{Nav, Navbar, NavDropdown } from "react-bootstrap";
+
+export default function NavBar(props) {
+    const user = props.user;
+
+    const [clicked, setClicked] = useState(false);
+
+
+    const handleClick = () => {
+        setClicked(!clicked);
+    }
+
+    return (
+            <nav className="NavbarItems">
+                <h1 className="navbar-logo">UCSB Food Bank<i className="fas fa-hamburger"></i></h1>
+                <div className="menu-icon" onClick={handleClick}>
+                    <i className={clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+                </div>
+                <ul className={clicked ? 'nav-menu active' : 'nav-menu'}>
+                    <Nav.Link href={"/"} className='nav-links'>Home</Nav.Link>
+                    <Nav.Link href={"/inventory"} className='nav-links'>Inventory</Nav.Link>
+                    {user && <Nav.Link href={"/"} className='nav-links'>Staff Portal</Nav.Link>}
+                    <Nav.Link href={"/contactus"} className='nav-links'>Contact Us</Nav.Link>
+                    {!user ? (
+                        <div className="nav-links-mobile" id="login-button" />) : (
+                        <NavDropdown className="nav-links-mobile"
+                            title={
+                            <span>
+                                Hello, {user.givenName}{" "}
+                                <img
+                                src={user.imageUrl}
+                                alt="profile"
+                                style={{ width: "24px", height: "24px" }}
+                                />{" "}
+                            </span>
+                            }
+                            id="basic-nav-dropdown"
+                        >
+                            <NavDropdown.Item className="nav-links-mobile" onClick={user.signOut}>
+                                Logout
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                        )}
+                </ul>
+            </nav>
+    )
+
+}
+
 // import React, { Component } from 'react';
 
 // import './Navbar.css'
@@ -32,59 +87,3 @@
 //         )
 //     }
 // }
-
-import React, { useState } from 'react';
-
-import './Navbar.css'
-import { Link } from 'react-router-dom';
-
-import Container from "react-bootstrap/Container";
-import{Nav, Navbar, NavDropdown } from "react-bootstrap";
-
-export default function NavBar(props) {
-    const user = props.user;
-
-    const [clicked, setClicked] = useState(false);
-
-
-    const handleClick = () => {
-        setClicked(!clicked);
-        console.log(clicked);
-    }
-
-    return (
-            <nav className="NavbarItems">
-                <h1 className="navbar-logo">UCSB Food Bank<i className="fas fa-hamburger"></i></h1>
-                <div className="menu-icon" onClick={handleClick}>
-                    <i className={clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
-                </div>
-                <ul className={clicked ? 'nav-menu active' : 'nav-menu'}>
-                    <Link to={"/"} className='nav-links'>Home</Link>
-                    <Link to={"/inventory"} className='nav-links'>Inventory</Link>
-                    {user && <Link to={"/"} className='nav-links'>Staff Portal</Link>}
-                    <Link to={"/contactus"} className='nav-links'>Contact Us</Link>
-                    {!user ? (
-                        <div className="nav-links-mobile" id="login-button" />) : (
-                        <NavDropdown
-                            title={
-                            <span>
-                                Hello, {user.fullName}{" "}
-                                <img
-                                src={user.imageUrl}
-                                alt="profile"
-                                style={{ width: "24px", height: "24px" }}
-                                />{" "}
-                            </span>
-                            }
-                            id="basic-nav-dropdown"
-                        >
-                            <NavDropdown.Item onClick={user.signOut}>
-                                Logout
-                            </NavDropdown.Item>
-                        </NavDropdown>
-                        )}
-                </ul>
-            </nav>
-    )
-
-}
