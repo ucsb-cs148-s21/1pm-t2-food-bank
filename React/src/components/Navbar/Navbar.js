aaf08832fdca184dc7ce9004a702d58af9195e5e
Liuhao Wu <liuhaowu@ucsb.edu>
2021-05-26 16:03:39 -0700

    frontend - only allow cetain emails to access staff portal

--- React/src/components/Navbar/Navbar.js
+++ React/src/components/Navbar/Navbar.js
@@ -34,7 +34,7 @@ export default function NavBar(props) {
                 <ul className={clicked ? 'nav-menu active' : 'nav-menu'}>
                     <Link to="/" className='nav-links'>Home</Link>
                     <Link to="/inventory" className='nav-links'>Inventory</Link>
-                    {user && <Link to="/" className='nav-links'>Staff Portal</Link>}
+                    {user && user.Admin && <Link to="/" className='nav-links'>Staff Portal</Link>}
                     <Link to="/contactus" className='nav-links'>Contact Us</Link>
                     {!user ? (
                         <div className="nav-links-mobile" id="login-button" />) : (
@@ -61,38 +61,3 @@ export default function NavBar(props) {
     )
 
 }
-
-// import React, { Component } from 'react';
-
-// import './Navbar.css'
-// import { Link } from 'react-router-dom';
-
-
-// export default class Navbar extends Component {
-
-//     state = { clicked: false}
-
-//     handleClick = () => {
-//         this.setState({ clicked: !this.state.clicked })
-//     }
-
-    
-
-//     render() {
-//         return (
-//             <nav className="NavbarItems">
-//                 <h1 className="navbar-logo">UCSB Food Bank<i className="fas fa-hamburger"></i></h1>
-//                 <div className="menu-icon" onClick={this.handleClick}>
-//                     <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
-//                 </div>
-//                 <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
-//                     <Link to={"/"} className='nav-links'>Home</Link>
-//                     <Link to={"/inventory"} className='nav-links'>Inventory</Link>
-//                     <Link to={"/"} className='nav-links'>Staff Portal</Link>
-//                     <Link to={"/contactus"} className='nav-links'>Contact Us</Link>
-//                     <Link to={"/login"} className='nav-links-mobile'>Login</Link>
-//                 </ul>
-//             </nav>
-//         )
-//     }
-// }
