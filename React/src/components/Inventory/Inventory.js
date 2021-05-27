174f2555b0573fb3e10647a7d2a40415b64b0395
Sunirse Gao <sunrise.stack@gmail.com>
2021-05-27 16:52:34 -0700

    timestamp - minor

--- React/src/components/Inventory/Inventory.js
+++ React/src/components/Inventory/Inventory.js
@@ -1,36 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import BootstrapTable from 'react-bootstrap-table-next';
 import './Inventory.css'
 
-const columns = [{
-  dataField: 'catagory',
-  text: 'Catagory'
-  
-},
-{
-  dataField: 'name',
-  text: 'Inventory'
-},
-{
-  dataField: 'amount',
-  text: 'Quantity'
-},
-{  
-  dataField: 'limit',
-  text: 'Limit'
-}];
+import Datatable from "./datatable"
 
+// https://www.youtube.com/watch?v=d1r0aK5awWk
+// require("es6-promise").polyfill();
+// require("isomorphic-fetch")
 
 export default class Inventory extends Component {
+
   constructor(props) {
     super(props);
-    
+    // const [data, setData] = useState([]);
+    // const [q, setQ] = useState("")
     this.state = {
-      inventory: null,
+      data: null,
       loading: true,
-      error: null
+      error: null,
+      q: "",
     };
-
   }
 
   async componentDidMount() {
@@ -38,14 +27,22 @@ export default class Inventory extends Component {
     const response = await fetch(API);
     try {
       const data = await response.json();
-      this.setState({ inventory: data, loading: false });
+      this.setState({ data: data, loading: false });
     } catch (e) { this.setState({ error: e }) }
 
     fetch ("api/getTime/last update")
-      .then(response => response.json)
-      .then(time => this.setState({ time }));
+    .then(response => response.json)
+    .then(time => this.setState({ time }));
+
   }
 
+  setQ(value){
+    this.setState({ q: value });
+  }
+  search(rows){
+    return rows.filter( row => row.name.toLowerCase().indexOf(this.state.q) > -1 || row.catagory.toLowerCase().indexOf(this.state.q) > -1)
+  }
+  
   render() {
     if (this.state.error) {
       return <p>{this.state.error.message}</p>;
@@ -54,12 +51,7 @@ export default class Inventory extends Component {
       return <p>Loading ...</p>;
     }
     return (
-      <div className='inventoryTable'>
-          <BootstrapTable  keyField='catagory' data={this.state.inventory || []} columns={columns} />
-
       <div>
-      
-
         <label className= 'searchLabel' for="search">Search:</label>
         <input className= 'searchInput' type="text" id="search" value={this.state.q} onChange={(e) => this.setQ(e.target.value)}></input>
 
@@ -69,6 +61,7 @@ export default class Inventory extends Component {
           <Datatable data={this.search(this.state.data)} />
         </div>
       </div>
+
     );
   }
 }
