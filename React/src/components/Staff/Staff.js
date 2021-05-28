520ea81d2dd0cb5a9399b8f8a3d07771345fa97f
kaiwenli1 <72473351+kaiwenli1@users.noreply.github.com>
2021-05-27 23:24:02 -0700

    Integrate index.js into staff.js

--- React/src/components/Staff/Staff.js
+++ React/src/components/Staff/Staff.js
@@ -5,6 +5,14 @@ import getUser from "../../utils/get-user";
 import Layout from '../../pages/Layout'
 
 import Datatable from "./datatable"
+import axios from 'axios'
+
+
+
+
+// end
+
+
 
 // https://www.youtube.com/watch?v=d1r0aK5awWk
 // require("es6-promise").polyfill();
@@ -30,6 +38,8 @@ class Staff extends Component {
       loading: true,
       error: null,
       q: "",
+      columns: ['name', 'limit', 'amount', 'catagory'],
+      update: 'true'
     };
   }
 
@@ -40,7 +50,30 @@ class Staff extends Component {
       const data = await response.json();
       this.setState({ data: data, loading: false });
     } catch (e) { this.setState({ error: e }) }
+    console.log("update: ", this.state.loading);
   }
+
+  // async componentWillMount() {
+  //   const API = "/api/getInventory";
+  //   const response = await fetch(API);
+  //   try {
+  //     const data = await response.json();
+  //     this.setState({ data: data, loading: false });
+  //   } catch (e) { this.setState({ error: e }) }
+  //   console.log("update: ", this.state.loading);
+  // }
+
+  // async componentDidUpdate(prevProps, prevState) {
+  //   if (prevState.loading !== this.state.loading) {
+  //       const API = "/api/getInventory";
+  //     const response = await fetch(API);
+  //     try {
+  //       const data = await response.json();
+  //       this.setState({ data: data, loading: false });
+  //     } catch (e) { this.setState({ error: e }) }
+  //     console.log("update: ", this.state.loading);
+  //   }
+  // }
 
   setQ(value){
     this.setState({ q: value });
@@ -48,7 +81,85 @@ class Staff extends Component {
   search(rows){
     return rows.filter( row => row.name.indexOf(this.state.q) > -1 || row.catagory.indexOf(this.state.q) > -1)
   }
-  
+
+  onCreate(){
+    var name = document.getElementById('name').value
+    var limit = document.getElementById('limit').value
+    var amount = document.getElementById('amount').value
+    var category = document.getElementById('category').value
+    if (limit === '' || amount === '' || name === '' || category === '' ){
+        window.alert('Invalid input!')
+    }
+    else {
+        const item = {
+            'name': name,
+            'limit': limit,
+            'amount': amount,
+            'catagory': category
+        }
+        axios.post('api/addInventory', item)
+        .then(function (response) {
+            console.log(response);
+            window.alert('Item Created!')
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+    }
+    this.setState({update: this.state.update});
+    console.log("update state", this.state.update);
+  }
+
+  // tutorial: https://www.javatpoint.com/react-axios-delete-request-example
+  onDelete(name){
+    if (name === ''){
+        window.alert('Invalid item!')
+    }
+    else {
+        axios.delete('api/deleteInventory/'+ name)
+        .then(function (response) {
+            console.log(response);
+            window.alert('Item Deleted!')
+            //console.log(response.data);
+            //const posts = data.filter(item => item.name !== name);
+            //this.setState({ posts });  
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+    }
+    this.setState({loading: true});
+    console.log("update state", this.state.loading);
+  }
+
+  onUpdate(){
+    var name = document.getElementById('name').value
+    var limit = document.getElementById('limit').value
+    var amount = document.getElementById('amount').value
+    var category = document.getElementById('category').value
+    if (limit === '' || amount === '' || name === '' || category === '' ){
+        window.alert('Invalid input!')
+    }
+    else {
+        const item = {
+            'name': name,
+            'limit': limit,
+            'amount': amount,
+            'catagory': category
+        }
+        axios.put('api/updateInventory', item)
+        .then(function (response) {
+            console.log(response);
+            window.alert('Item Updated!')
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+    }
+    this.setState({update: !this.state.update});
+    console.log(this.state.update);
+  }
+
   render() {
     if (this.state.error) {
       return <p>{this.state.error.message}</p>;
@@ -57,13 +168,32 @@ class Staff extends Component {
       return <p>Loading ...</p>;
     }
     return (
-
         <div>
           <label className= 'searchLabel' for="search">Search:</label>
           <input className= 'searchInput' type="text" id="search" value={this.state.q} onChange={(e) => this.setQ(e.target.value)}></input>
 
           <div name="staffTable">
-            <Datatable data={this.search(this.state.data)} />
+            <table cellPadding={0} cellSpacing={0}>
+              <thead>
+                <tr>{this.state.data[0] && this.state.columns.map(heading => <th>{heading}</th>)}</tr>
+              </thead>
+                <tbody>
+                    {this.state.data.map(row => <tr>
+                        {
+                            this.state.columns.map(column => <td>{row[column]}</td>)
+                        }
+                        <td><button onClick={this.onUpdate}>Update</button></td>
+                        <td><button onClick={() => this.onDelete(row.name)}>Delete</button></td>
+                    </tr>)}
+                    <tr>
+                    <td><input type="text" id="name" name="fname" required/></td>
+                    <td><input type="number" id="limit" name="flimit" min="1" required/></td>
+                    <td><input type="number" id="amount" name="famount" min="0" required/></td>
+                    <td><input type="text" id="category" name="fcategory" required/></td>
+                    <td><button onClick={this.onCreate}>Create</button></td>
+                    </tr>
+                </tbody>
+            </table>
           </div>
         </div>
 
--- React/src/components/Staff/datatable/index.js
+++ React/src/components/Staff/datatable/index.js
@@ -80,7 +80,8 @@ function onCreate(){
 export default function Datatable({data})
 {
     const columns = data[0] && Object.keys(data[0]);
-    return (<table cellPadding={0} cellSpacing={0}>
+    return (
+    <table cellPadding={0} cellSpacing={0}>
         <thead>
             <tr>{data[0] && columns.map(heading => <th>{heading}</th>)}</tr>
         </thead>
