0c112ecb7ae3e208e84feea2b23d23fd04795f58
Orion Schwellenbach <jamesschwellenbach@ucsb.edu>
2021-06-03 15:33:16 -0700

    location of food bank is now clickable and opens the map location in a new page

--- React/src/components/footer/footer.js
+++ React/src/components/footer/footer.js
@@ -13,7 +13,7 @@ export function FooterContainer(){
                     </Footer.Column>
                     <Footer.Column>
                         <Footer.Title>Where</Footer.Title>
-                        <Footer.Link>Outside the Ortega side of the University Center, facing the lagoon.</Footer.Link>
+                        <Footer.Link href="https://www.google.com/maps?q=ucsb+food+bank&rlz=1C1GIGM_enUS869US869&um=1&ie=UTF-8&sa=X&ved=2ahUKEwiOksO8wfzwAhV2JTQIHaAsDJUQ_AUoAXoECAEQAw" target="_blank">Outside the Ortega side of the University Center, facing the lagoon.</Footer.Link>
                     </Footer.Column>
                     <Footer.Column>
                         <Footer.Title>Email us at</Footer.Title>
